662199a15554fe85ea64926de78431add17d5a71
Barbara Verburg <barb@zenzillo.com>
2019-03-08 16:52:51 -0500

    Move app to component folder

--- app/index.js
+++ app/index.js
@@ -1,17 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
 require('./index.css');
-
-class App extends React.Component {
-	render() {
-		return (
-		    <div>
-		    	Hello World!
-		    </div>
-		)
-	}
-}
+var App = require('./components/App');
 
 ReactDOM.render(
 	<App />,
