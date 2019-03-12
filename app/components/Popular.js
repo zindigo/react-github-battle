d31fc10ae2f7ed5d5973785b42d0e77387d29832
Barbara Verburg <barb@zenzillo.com>
2019-03-12 12:36:22 -0400

    Fetch repos from github api

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage (props) {
 	var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -19,6 +20,35 @@ function SelectLanguage (props) {
    )
 }
 
+function RepoGrid (props) {
+	return (
+		<ul className='popular-list'>
+			{props.repos.map(function(repo, index) {
+				return (
+					<li key={repo.name} className='popular-item'>
+						<div className='popular-rank'>#{index + 1}</div>
+						<ul className='space-list-items'>
+							<li>
+								<img
+									className='avatar'
+									src={repo.owner.avatar_url}
+									alt={'Avatar for '+repo.owner.login} />
+							</li>
+							<li><a href={repo.html_url}>{repo.name}</a></li>
+							<li>@{repo.owner.login}</li>
+							<li>{repo.stargazers_count} stars</li>
+						</ul>
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
+
+RepoGrid.proptypes = {
+	repos: PropTypes.array.isRequired,
+}
+
 SelectLanguage.propTypes = {
 	selectedLanguage: PropTypes.string.isRequired,
 	onSelect: PropTypes.func.isRequired,
@@ -28,18 +58,32 @@ class Popular extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state =  {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		};
 
 		this.updateLanguage = this.updateLanguage.bind(this);
 	}
-
+	componentDidMount() {
+		this.updateLanguage(this.state.selectedLanguage);
+	}
 	updateLanguage(lang) {
 		this.setState(function () {
 			return {
-				selectedLanguage: lang
+				selectedLanguage: lang,
+				repos: null
 			}
 		});
+
+		// ajax request
+		api.fetchPopularRepos(lang)
+			.then(function(repos) {
+				this.setState(function () {
+					return {
+						repos: repos
+					}
+				})
+			}.bind(this));
 	}
 
 	render() {
@@ -49,6 +93,9 @@ class Popular extends React.Component {
 	        		selectedLanguage={this.state.selectedLanguage}
 	        		onSelect={this.updateLanguage}
 	        	/>
+	        	{!this.state.repos
+	        		? <p>LOADING</p>
+	        		: <RepoGrid repos={this.state.repos} />}
 	        </div>
 		)
 	}
