77d16822072e2042557fd76d51127119e48ece13
Barbara Verburg <barb@zenzillo.com>
2019-05-21 16:06:20 -0400

    Refactor to use class properties

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -9,12 +9,17 @@ const styles = {
 }
 
 class Loading extends React.Component {
-	constructor(props) {
-		super(props);
+	static propTypes = {
+		text: PropTypes.string.isRequired,
+		speed: PropTypes.number.isRequired,
+	};
 
-		this.state = {
-			text: props.text
-		};
+	static defaultProps = {
+		text: 'Loading',
+		speed: 300,
+	};
+	state = {
+		text: this.props.text
 	}
 	componentDidMount() {
 		const { text, speed } = this.props
@@ -37,15 +42,5 @@ class Loading extends React.Component {
 		)
 	}
 }
-
-Loading.propTypes = {
-	text: PropTypes.string.isRequired,
-	speed: PropTypes.number.isRequired,
-};
-
-Loading.defaultProps = {
-	text: 'Loading',
-	speed: 300,
-};
 
 export default Loading;
--- app/components/Results.js
+++ app/components/Results.js
@@ -39,17 +39,12 @@ Player.propTypes =  {
 }
 
 class Results extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state =  {
-			winner: null,
-			loser: null,
-			error: null,
-			loading: true
-		}
+	state = {
+		winner: null,
+		loser: null,
+		error: null,
+		loading: true
 	}
-
 	componentDidMount() {
 		const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
 		battle([
