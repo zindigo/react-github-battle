ab510f2fe5ca4634ff7d353bcd065cbd089404bf
Barbara Verburg <barb@zenzillo.com>
2019-05-21 14:47:55 -0400

    Refactor import/export for ES6

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,7 @@
-const React = require('react');
-const PropTypes = require('prop-types');
-const api = require('../utils/api');
-const Loading = require('./Loading');
+import React from 'react';
+import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
+import Loading from './Loading';
 
 function SelectLanguage ({ selectedLanguage, onSelect }) {
 	const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -68,7 +68,7 @@ class Popular extends React.Component {
 		this.setState(() => ({ selectedLanguage: lang, repos: null }))
 
 		// ajax request
-		api.fetchPopularRepos(lang)
+		fetchPopularRepos(lang)
 			.then((repos) => this.setState(() => ({ repos })));
 	}
 
@@ -89,4 +89,4 @@ class Popular extends React.Component {
 	}
 }
 
-module.exports = Popular;
+export default Popular;
