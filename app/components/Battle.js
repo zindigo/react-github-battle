847303e49bfcaaf82fe804a39dbaea4a349a29e5
Barbara Verburg <barb@zenzillo.com>
2019-05-21 14:26:42 -0400

    Refactor to ES6

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,7 +1,7 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
+const React = require('react');
+const PropTypes = require('prop-types');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
 	constructor(props) {
@@ -15,13 +15,9 @@ class PlayerInput extends React.Component {
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	handleChange(event) {
-		var value = event.target.value;
+		const value = event.target.value;
 
-		this.setState(function() {
-			return {
-				username: value
-			}
-		})
+		this.setState(() => ({ username: value }))
 	}
 	handleSubmit(event) {
 		event.preventDefault();
@@ -32,23 +28,26 @@ class PlayerInput extends React.Component {
 		)
 	}
 	render() {
+		const { username } = this.state
+		const { label } = this.props
+
 		return (
 		    <form className='column' onSubmit={this.handleSubmit}>
 		    	<label className='header' htmlFor='username'>
-		    		{this.props.label}
+		    		{label}
 		    	</label>
 		    	<input
 		    		id='username'
 		    		placeholder='github username'
 		    		type='text'
 		    		autoComplete='off'
-		    		value={this.state.username}
+		    		value={username}
 		    		onChange={this.handleChange}
 		    	/>
 		    	<button
 		    		className='button'
 		    		type='submit'
-		    		disabled={!this.state.username}>
+		    		disabled={!username}>
 		    			Submit
 		    	</button>
 		    </form>
@@ -79,29 +78,22 @@ class Battle extends React.Component {
 	}
 
 	handleSubmit(id, username) {
-		this.setState(function() {
-			var newState = {};
-			newState[id + 'Name'] = username;
-			newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
-			return newState;
-		});
+		this.setState(() => ({
+			[id + 'Name']: username,
+			[id + 'Image']: `https://github.com/${username}.png?size=200`
+		}))
 	}
 
 	handleReset(id) {
-		this.setState(function() {
-			var newState = {};
-			newState[id + 'Name'] = '';
-			newState[id + 'Image'] = null;
-			return newState;
-		});
+		this.setState(() => ({
+			[id + 'Name']: '',
+			[id + 'Image']: null
+		}))
 	}
 
 	render() {
-		var match = this.props.match;
-		var playerOneName = this.state.playerOneName;
-		var playerTwoName = this.state.playerTwoName;
-		var playerOneImage = this.state.playerOneImage;
-		var playerTwoImage = this.state.playerTwoImage;
+		const { match } = this.props;
+		const { playerOneName, playerOneImage, playerTwoName, playerTwoImage } = this.state;
 
 		return (
 		  <div>
@@ -119,7 +111,7 @@ class Battle extends React.Component {
 		     			username={playerOneName}>
 				    	<button
 				    		className='reset'
-				    		onClick={this.handleReset.bind(null, 'playerOne')}>
+				    		onClick={() => this.handleReset('playerOne')}>
 				    		Reset
 				    	</button>
 		     		</PlayerPreview>}
@@ -137,7 +129,7 @@ class Battle extends React.Component {
 		     			username={playerTwoName}>
 		     			<button
 				    		className='reset'
-				    		onClick={this.handleReset.bind(null, 'playerTwo')}>
+				    		onClick={() => this.handleReset('playerTwo')}>
 				    		Reset
 				    	</button>
 		     		</PlayerPreview>}
@@ -148,7 +140,7 @@ class Battle extends React.Component {
 		     		className='button'
 		     		to={{
 		     			pathname: match.url + '/results',
-		     			search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName,
+		     			search: `?playerOneName=${playerOneName}&playerTwoName=${playerTwoName}`,
 		     		}}>
 		     		Battle
 		     	</Link>
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,14 +1,12 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
+const React = require('react');
+const PropTypes = require('prop-types');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
-function Profile (props) {
-	var info = props.info;
-
+function Profile ({ info }) {
 	return (
 	     <PlayerPreview avatar={info.avatar_url} username={info.login}>
 			<ul className='space-list-items'>
@@ -24,12 +22,12 @@ function Profile (props) {
 	)
 }
 
-function Player(props) {
+function Player({ label, score, profile }) {
 	return (
 	    <div>
-	    	<h1 className='header'>{props.label}</h1>
-	    	<h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-	    	<Profile info={props.profile} />
+	    	<h1 className='header'>{label}</h1>
+	    	<h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+	    	<Profile info={profile} />
 	    </div>
 	)
 }
@@ -53,35 +51,28 @@ class Results extends React.Component {
 	}
 
 	componentDidMount() {
-		var players = queryString.parse(this.props.location.search);
+		const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
 		api.battle([
-		    players.playerOneName,
-		    players.playerTwoName
-		]).then(function (results) {
+		    playerOneName,
+		    playerTwoName
+		]).then((results) => {
 			if (results === null) {
-				return this.setState(function() {
-					return {
-						error: 'Looks like there was an error. Check that both users exist on Github',
-						loading: false,
-					}
-				});
+				return this.setState(() => ({
+					error: 'Looks like there was an error. Check that both users exist on Github',
+					loading: false,
+				}));
 			}
 
-			this.setState(function() {
-				return {
-					error: null,
-					winner: results[0],
-					loser: results[1],
-					loading: false
-				}
-			});
-		}.bind(this));
+			this.setState(() => ({
+				error: null,
+				winner: results[0],
+				loser: results[1],
+				loading: false
+			}));
+		});
 	}
 	render() {
-		var error = this.state.error;
-		var winner = this.state.winner;
-		var loser = this.state.loser;
-		var loading = this.state.loading;
+		const { error, winner, loser, loading } = this.state
 
 		if (loading === true) {
 			return <Loading />
